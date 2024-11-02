4ee10e3c82e8bd8068bf1c780ed19e483fb2daf3
kietn20 <kietnguyen3698@gmail.com>
2024-11-01 19:47:22 -0700

    setting up client

--- server/index.js
+++ server/index.js
@@ -1,7 +1,7 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 const app = express();
 app.use(cors());
