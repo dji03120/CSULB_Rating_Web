909c6618b0fc66c114f077c08129f83f5de00026
kietn20 <kietnguyen3698@gmail.com>
2024-11-02 14:12:14 -0700

    working on API routes

---
+++ server/src/models/Users.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    studentId: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+export const UserModel = mongoose.model('users', UserSchema);
---
+++ server/src/routes/users.js
@@ -0,0 +1,11 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { UserModel } from "../models/Users.js";
+
+const router = express.Router();
+
+
+
+
+
