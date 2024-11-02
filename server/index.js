8913f3c4fb5b9fdbc5dbecd525e354532b93fb72
kietn20 <kietnguyen3698@gmail.com>
2024-11-01 22:35:22 -0700

    working on backend

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cors from 'cors';
+
 import dotenv from 'dotenv';
+dotenv.config();
 
 const app = express();
 app.use(cors());
@@ -27,5 +29,3 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
-
-app.use(express.json());
