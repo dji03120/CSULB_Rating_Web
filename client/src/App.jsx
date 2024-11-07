3e9e48a43c11c42d931d4de40de1f39e72c0dec1
Veronica Nguyen <veronicanguyen78@gmail.com>
2024-11-07 12:36:27 -0800

    Create register/login page
    Still need to test in database

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import { Auth } from "./pages/auth";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import { Home } from './components/home';
@@ -16,7 +17,7 @@ function App() {
           <Route path="/" element={<Home />} />
           <Route path="/create-post" element={<></>} />
           <Route path="/saved-posts" element={<></>} />
-          <Route path="/auth" element={<></>} />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </Router>
     </div>
---
+++ client/src/pages/auth.jsx
@@ -0,0 +1,120 @@
+import { useState } from 'react';
+import axios from "axios";
+import { useCookies } from 'react-cookie'
+import { useNavigate, userNavigate } from 'react-router-dom'
+
+export const Auth = () => {
+    return (
+    <div className="auth">
+        <Login />
+        <Register />
+    </div>
+    );
+}
+
+const Login = () => { 
+    const [studentId, setStudentId] = useState("")
+    const [password, setPassword] = useState("")
+    const [_, setCookies] = useCookies(["access_token"])
+    const navigate = useNavigate()
+
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await axios.post("http://localhost:3001/auth/login", {
+                studentId,
+                password,
+            });
+
+            setCookies("access_token", response.data.token)
+            window.localStorage.setItem("userId", response.data.user._id)
+            navigate("/")
+        } catch (error) {
+            console.error(error);
+            alert("Login failed");
+        }
+    };
+
+    return (
+        <Form 
+            studentId={studentId} 
+            setStudentId={setStudentId} 
+            password={password} 
+            setPassword={setPassword}
+            label="Login"
+            onSubmit={onSubmit}
+        />
+    );
+};
+
+const Register = () => {
+    const [studentId, setStudentId] = useState("")
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    
+    const onSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            await axios.post("http://localhost:3001/auth/register", {
+                studentId,
+                email,
+                password,
+            });
+            alert("Registration complete")
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    return (
+        <Form 
+            studentId={studentId}
+            setStudentId={setStudentId}
+            email={email}
+            setEmail={setEmail} 
+            password={password} 
+            setPassword={setPassword}
+            label="Register"
+            onSubmit={onSubmit}
+        />
+    );
+};
+
+// Creates form for register and login
+const Form = ({ studentId, setStudentId, email, setEmail, password, setPassword, label, onSubmit }) => {
+    return (
+        <form onSubmit={onSubmit}>
+            <h2>{label}</h2>
+            <div className="form-group">
+                <label htmlFor="studentId">Student ID: </label>
+                <input
+                    type="text" 
+                    id="studentId" 
+                    value={studentId}
+                    onChange={(event) => setStudentId(event.target.value)}/>
+            </div>
+            {label === "Register" && (
+                <div className="form-group">
+                    <label htmlFor="email">Email: </label>
+                    <input 
+                        type="text" 
+                        id="email" 
+                        value={email}
+                        onChange={(event) => setEmail(event.target.value)}/>
+                </div>
+            )}
+            <div className="form-group">
+                <label htmlFor="password">Password: </label>
+                <input 
+                    type="text" 
+                    id="password" 
+                    value={password}
+                    onChange={(event) => setPassword(event.target.value)}/>
+            </div>
+            {/* Submits the form when button is clicked */}
+            <button type="submit">{label}</button>
+        </form>
+    );
+};
