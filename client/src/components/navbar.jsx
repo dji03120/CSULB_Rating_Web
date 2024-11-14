a330bf6741547f70e33a14c68fd3eda1aebac0f7
Veronica Nguyen <veronicanguyen78@gmail.com>
2024-11-14 00:07:45 -0800

    Navbar button clicks direct to pages

--- client/src/components/navbar.jsx
+++ client/src/components/navbar.jsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useNavigate } from "react-router-dom"
 import "./navbar.css";
 
 const Navbar = () => {
+	const navigate = useNavigate();
+
+	const gotoHomepage = () => navigate("/")  // Navigates to homepage
+	const goToLogin = () => navigate("/login")  // Navigates to login page
+	const goToRegister = () => navigate("/register")  // Navigates to register page
+	
 	return (
 		<nav className="navbar">
 			<div className="navbar-content">
-				<h1 className="navbar-title">
+				<h1 className="navbar-title" onClick={gotoHomepage}>
 					<img
 						src="src/assets/star.png"
 						alt="Logo"
@@ -14,8 +21,8 @@ const Navbar = () => {
 					CSULB Rates
 				</h1>
 				<div className="navbar-buttons">
-					<button className="navbar-button">Login</button>
-					<button className="navbar-button">Register</button>
+					<button className="navbar-button" onClick={goToLogin}>Login</button>
+					<button className="navbar-button" onClick={goToRegister}>Register</button>
 				</div>
 			</div>
 		</nav>
