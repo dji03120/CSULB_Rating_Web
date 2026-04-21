d6e4db2ef7e407c5ec4e52805f795b313ff98122
dji03120 <dji03120@naver.com>
2026-04-21 17:33:04 +0900

    Update index.js

--- server/index.js
+++ server/index.js
@@ -29,8 +29,10 @@ app.use(cors({
 }));
 app.use(express.json());
 
+console.log("MONGODB_URI:", process.env.MONGODB_URI)
+
 // Database connection
-mongoose.connect(process.env.MONGODB_URI, {
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true
 })
